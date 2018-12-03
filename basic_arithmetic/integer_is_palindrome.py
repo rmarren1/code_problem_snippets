60a77522ec8e4a1d892d6fa57dc87799b7276fcd
Ryan Marren <rymarr@tuta.io>
2018-12-02 20:16:47 -0500

    integer palindrome check with O(1) space

--- basic_arithmetic/integer_is_palindrome.py
+++ basic_arithmetic/integer_is_palindrome.py
@@ -1,7 +1,13 @@
+import math
+
 def check_palindrome(x):
     """
     return True if the string representation of x is a palindrome
     return False otherwise
+
+    Time complexity - O(n)
+    Space complexity - O(n)
+    where n is the number of digits in x
     """
     # If x is negative, its representation starts with a '-'
     # so it can't be a palindrome
@@ -14,13 +20,51 @@ def check_palindrome(x):
             return False
     return True
 
-assert check_palindrome(0)
-assert check_palindrome(1)
+def check_palindrome_no_string(x):
+    """
+    return True if the string representation of x is a palindrome
+    return False otherwise
+    Do not convert to a string
+
+    Time complexity - O(n)
+    Space complexity - O(1)
+    where n is the number of digits in x
+    """
+    def _extract_digit(x, i):
+        return (x // 10**(i)) % 10
+
+    # If x is negative, its representation starts with a '-'
+    # so it can't be a palindrome
+    # since an integer will never end with a '-'
+    if x < 0:
+        return False
+    # Need this check since math.log(0) is undefined
+    if x == 0:
+        return True
+    n = int(math.floor(math.log(x, 10)) + 1)
+    for i in range(n // 2):
+        if  _extract_digit(x, i) != _extract_digit(x, n - i - 1):
+            return False
+    return True
+
+
+assert     check_palindrome(0)
+assert     check_palindrome(1)
 assert not check_palindrome(-1)
-assert check_palindrome(11)
-assert check_palindrome(121)
+assert     check_palindrome(11)
+assert     check_palindrome(121)
 assert not check_palindrome(-121)
-assert check_palindrome(12321)
-assert check_palindrome(12345678987654321)
+assert     check_palindrome(12321)
+assert     check_palindrome(12345678987654321)
 assert not check_palindrome(123432)
+
+assert     check_palindrome_no_string(0)
+assert     check_palindrome_no_string(1)
+assert not check_palindrome_no_string(-1)
+assert     check_palindrome_no_string(11)
+assert     check_palindrome_no_string(121)
+assert not check_palindrome_no_string(-121)
+assert     check_palindrome_no_string(12321)
+assert     check_palindrome_no_string(12345678987654321)
+assert not check_palindrome_no_string(123432)
 
