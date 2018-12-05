0ab44fa41ec3708c38d507ebf52f384cb1ffdeb4
Ryan Marren <rymarr@tuta.io>
2018-12-04 22:31:15 -0500

    fix multiply

--- arrays/multiply.py
+++ arrays/multiply.py
@@ -12,30 +12,31 @@ def multiply(A, B):
    1 4 4
 
     """
-    m = A if len(A) <= len(B) else B
-    M = A if len(A) > len(B) else B
-    s = [0] * (len(m) + len(M))
-    m.reverse()
-    M.reverse()
-    for i in range(len(m)):
-        for j in range(len(M)):
-            s[i + j] += m[i] * M[j]
-    for i in range(len(s) - 1):
-        tmp = s[i]
-        s[i] = tmp % 10
-        s[i + 1] += tmp // 10
-    s.reverse()
+    negative = True if (A[0] < 0) ^ (B[0] < 0) else False
+    A[0] = abs(A[0])
+    B[0] = abs(B[0])
+    C = [0] * (len(A) + len(B))
+    for i in reversed(range(len(A))):
+        for j in reversed(range(len(B))):
+            C[i + j + 1] += A[i] * B[j]
+            C[i + j] += C[i + j + 1] // 10
+            C[i + j + 1] %= 10
     i = 0
-    while i < len(s) - 1 and s[i] == 0: i += 1
-    del s[:i]
-    return s
+    while i < len(C) - 1 and C[i] == 0: i += 1
+    del C[:i]
+    C[0] = -C[0] if negative else C[0]
+    return C
+
+
 
 print("Testing a lot of cases! Please wait!")
-for i in range(1000):
-    for j in range(1000):
+for i in range(0, 100):
+    for j in range(0, 100):
         I = [int(x) for x in str(i)]
         J = [int(x) for x in str(j)]
         IJ = [int(x) for x in str(i * j)]
         assert multiply(I[:], J[:]) == IJ
 
-
+assert multiply([-1, 0], [1, 0]) == [-1, 0, 0]
+assert multiply([-1, 0], [-1, 0]) == [1, 0, 0]
+assert multiply([-9, 9], [9, 9]) == [-9, 8, 0, 1]
